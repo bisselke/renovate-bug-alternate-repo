2aa6ee07264c699d2c2e4e56d814d8da984e9a94
r.bissels <r.bissels@everest.nl>
2025-02-20 13:51:30 +0100

    add example project for renovate alternate repo bug

---
+++ src/main/java/com/bisselke/renovate_bug_alternate_repo/RenovateBugAlternateRepoApplication.java
@@ -0,0 +1,13 @@
+package com.bisselke.renovate_bug_alternate_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateBugAlternateRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovateBugAlternateRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bisselke/renovate_bug_alternate_repo/RenovateBugAlternateRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisselke.renovate_bug_alternate_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateBugAlternateRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
